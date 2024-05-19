1298e0a77685dbd0c4c19a196dbcfb4142b0a438
Patrick Odeh <vyltyan@gmail.com>
2024-05-19 10:16:58 -0700

    tests/

--- models/__init__.py
+++ models/__init__.py
@@ -15,6 +15,7 @@ def register_class(cls):
     """Register a class in the class registry"""
     class_registry[cls.__name__] = cls
 
+
 def get_class(name):
     """get a class from the class registry"""
     return class_registry.get(name)
